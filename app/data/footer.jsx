5cfcd1b2cb596263f08a8baddfee3d32bcf488d5
dalienst <oduordalienst@gmail.com>
2024-06-19 20:04:56 +0300

    add footer

---
+++ app/data/footer.jsx
@@ -0,0 +1,89 @@
+const legals = [
+    {
+        id: 1,
+        title: "Privacy Policy",
+    },
+    {
+        id: 2,
+        title: "Terms and Conditions",
+    },
+    {
+        id: 3,
+        title: "Security Policy",
+    },
+    {
+        id: 4,
+        title: "Personal Data Policy",
+    },
+    {
+        id: 5,
+        title: "Member Rights",
+    },
+]
+
+const company = [
+    {
+        id: 1,
+        title: "About Us",
+    },
+    {
+        id: 2,
+        title: "Join us",
+    },
+    {
+        id: 3,
+        title: "Web Application",
+    },
+    {
+        id: 4,
+        title: "Customer Support",
+    },
+    {
+        id: 5,
+        title: "Contact Us",
+    },
+]
+
+const loans = [
+    {
+        id: 1,
+        title: "Asset Finance",
+    },
+    {
+        id: 2,
+        title: "Personal Loans",
+    },
+    {
+        id: 3,
+        title: "Business Loans",
+    },
+    {
+        id: 4,
+        title: "Emergency Loans",
+    },
+]
+
+const quickLinks = [
+    {
+        id: 1,
+        title: "Help and Support",
+    },
+    {
+        id: 2,
+        title: "Claims",
+    },
+    {
+        id: 3,
+        title: "Resources",
+    },
+    {
+        id: 4,
+        title: "Portal",
+    },
+    {
+        id: 5,
+        title: "Contact",
+    },
+]
+
+export { legals, company, loans, quickLinks }
