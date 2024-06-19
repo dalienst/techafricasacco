50871c9891e443bd64fce26dd7820650ffd3adc5
dalienst <oduordalienst@gmail.com>
2024-06-19 13:25:31 +0300

    fix cards ui

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -29,7 +29,7 @@ function About() {
                 {aboutSummary?.map((item) => (
                   <div className="col-md-4 col-sm-12 mb-3 h-100" key={item.id}>
                     <div
-                      className="card rounded-3"
+                      className="card rounded-3 h-100"
                       style={{ backgroundColor: "#f2ecf6" }}
                     >
                       <div className="card-body">
